67262c66f79d4d1e926df2d58751e0ebbb70be0a
Nico Rithner <nicorithner@gmail.com>
2022-04-18 13:55:05 -0600

    Complete exercise 1

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = false;
-  this.info = function() {
-    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${ this.read ? 'read' : 'not read yet'}`)
-  }
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? 'read' : 'not read yet'
+    }`;
+  };
 }
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+console.log(theHobbit.info());
