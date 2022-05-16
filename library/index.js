30fd1838850b7dc16f2b40a6fc876fcd847a66ec
Nico Rithner <nicorithner@gmail.com>
2022-05-16 07:29:19 -0600

    Display book info

--- library/index.js
+++ library/index.js
@@ -1,10 +1,12 @@
+const booksList = document.querySelector('.booksList');
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = false;
+  this.read = read || false;
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
       this.read ? "read" : "not read yet"
@@ -12,7 +14,18 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
+for (let i = 1; i <= 5; i++) {
+ addBookToLibrary( new Book(`Title ${i}`, 'Nico', Math.floor(Math.random() * 100), true) )
+}
+
+console.log(myLibrary)
+
+// display books list
+
+myLibrary.forEach(book => {
+  booksList.innerHTML += `<p>${book.info()}</p>`
+});
